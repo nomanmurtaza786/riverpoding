d23b145ed3c81b3024aebe7a51de49a37da1bf42
Noman Murtaza <Norman.Murtaza@tabcorp.com.au>
2023-03-08 19:30:15 +1030

    infinite loading list

--- lib/screens/passengers_screen.dart
+++ lib/screens/passengers_screen.dart
@@ -12,25 +12,30 @@ class PassengersScreen extends ConsumerWidget {
       appBar: AppBar(
         title: const Text('Passengers'),
       ),
-      body: ref.watch(fetchPassengersProvider(page: 0)).when(
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          final page = index ~/ 10;
+          final itemIndex = index % 10;
+          print('noman --->page $page');
+          //print('noman ---> item index $itemIndex');
+          final pageData = ref.watch(fetchPassengersProvider(page: page));
+          return pageData.when(
             data: (data) {
-              return RefreshIndicator(
-                onRefresh: () async {
-                  ref.invalidate(fetchPassengersProvider(page: 0));
-                },
-                child: ListView.builder(
-                  itemCount: data.data?.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Text(data.data?[index].name ?? '');
-                  },
-                ),
+              final passenger = data.data![itemIndex];
+              return ListTile(
+                title: Text(passenger.name!),
+                subtitle: Text('${passenger.trips} trips'),
               );
             },
-            error: (error, stack) => Center(child: Text(error.toString())),
-            loading: () => const Center(
-              child: CircularProgressIndicator(),
+            loading: () => const ListTile(
+              title: Text('Loading...'),
             ),
-          ),
+            error: (error, stack) => const ListTile(
+              title: Text('Error'),
+            ),
+          );
+        },
+      ),
     );
   }
 }
