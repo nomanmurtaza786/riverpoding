3aa6d24f7e43c6648c515f9b8714b280fcfeed46
Noman Murtaza <Norman.Murtaza@tabcorp.com.au>
2023-03-17 20:46:30 +1030

    dio refresh token

--- lib/interceptors/auth_interceptor.dart
+++ lib/interceptors/auth_interceptor.dart
@@ -1,15 +1,13 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:dio/dio.dart';
-import 'package:superwizor/local_persistence/local_storage.dart';
-import 'package:superwizor/local_persistence/local_storage_keys.dart';
+import 'package:superwizor/features/authentication/auth_manager.dart';
 
 class AuthInterceptor extends Interceptor {
   @override
   Future<void> onRequest(
       RequestOptions options, RequestInterceptorHandler handler) async {
-    final token = LocalStorage.instance
-        .getString(LocalStorageKeys.accessToken, defaultValue: 'DefaultToken');
-    options.headers['Authorization'] = token;
+    final token = AuthManager.instance.accessToken;
+    options.headers['Authorization'] = 'Bearer $token';
     return super.onRequest(options, handler);
   }
 }
--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -5,6 +5,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:superwizor/features/authentication/auth_manager.dart';
+import 'package:superwizor/interceptors/auth_interceptor.dart';
 import 'package:superwizor/models/activity_model.dart';
 import 'package:superwizor/services/api_services.dart';
 
@@ -18,44 +19,48 @@ List<ActivityModel> activityList(ActivityListRef ref) {
 //dio provider
 final dioClient = Provider(
   (ref) {
-    final accessToken = AuthManager.instance.accessToken;
-    final _dio = Dio(
-      BaseOptions(),
-    )
-      //   ..interceptors.add(
-      //     AuthInterceptor(),
-      //   )
+    final _dio = Dio()
+      ..interceptors.add(
+        AuthInterceptor(),
+      )
       ..interceptors.add(
         PrettyDioLogger(requestBody: true, requestHeader: true),
       );
     _dio.interceptors.add(InterceptorsWrapper(
       onError: (DioError e, handler) async {
         if (e.response?.statusCode == 401) {
-          print('noman --->' '401 error');
-          final options = e.response?.requestOptions;
-          print('noman --->' 'options: ${options?.path}}');
+          final options = e.response?.requestOptions.headers;
 
           // handle 401 error
           // for example, you can refresh the token and retry the request
-          await AuthManager.instance.saveAccessToken('nimi');
+          await AuthManager.instance.saveAccessToken('yoyo');
+
           //final newToken = await _dio.post('/refreshToken');
           final _accessToken = AuthManager.instance.accessToken;
+          final opts = Options(headers: {
+            'Authorization': 'Bearer ' + _accessToken,
+          });
 
-          // _dio.interceptors.responseLock.unlock();
-          //options.headers['Authorization'] = 'Bearer $newToken';
-          //_dio.options.headers['Authorization'] = 'Bearer 1212323124';
           final response = await _dio.request(
-              'https://api.instantwebtools.net/v1/passenger?page=0&size=10');
+              'https://api.instantwebtools.net/v1/passenger?page=0&size=10',
+              options: opts);
           handler.resolve(response);
+          ref.read(dioAuthProvider);
           return;
         }
       },
     ));
-    _dio.options.headers['Authorization'] = 'Bearer $accessToken';
 
     return _dio;
   },
 );
+
+@riverpod
+Dio dioAuth(DioAuthRef ref) {
+  print('noman --->' 'dio auth provide');
+  //ref.invalidate(dioClient);
+  return Dio();
+}
 
 @riverpod
 Future<List<ActivityModel>> fetchActivities(FetchActivitiesRef ref) async {
